69355f3d0fb96d3edf55774bfe833255c5b122b8
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-02-28 11:11:13 +0000

    added router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
-function App() {
+class App extends Component {
+  render() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+   <React.Fragment>
+<Router>
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={Home} />
+    <Route path="/contact" component={Home} />
+    <Route component={NoMatch} />
+    </Switch>
+    </Router>
+    </React.Fragment>
+    );
+  }
 }
 
 export default App;

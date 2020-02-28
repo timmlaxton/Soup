39babc3673f6258a7a0041e4fd280cb26b24b85c
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-02-28 11:48:31 +0000

    navbar renders to page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './Home';
 import Contact from './Contact';
 import About from './About';
+import Layout from './components/Layout';
+import Navbar from './components/NavigationBar';
+import NavigationBar from './components/NavigationBar';
 
 class App extends Component {
   render() {
   return (
    <React.Fragment>
+     <NavigationBar/>
+     <Layout>
 <Router>
   <Switch>
     <Route exact path="/" component={Home} />
@@ -15,6 +20,7 @@ class App extends Component {
     <Route path="/contact" component={Contact} />
     </Switch>
     </Router>
+    </Layout>
     </React.Fragment>
     );
   }
